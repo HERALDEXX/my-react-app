28c620f625932ed589937e5b5b5c4f3770ec170e
HERALDEXX <inyangheraldmbre@outlook.com>
2024-10-18 17:25:32 +0100

    Added score.js (If statement) and myForm.js (input)

--- src/Car.js
+++ src/Car.js
@@ -4,4 +4,23 @@ function Car() {
 /*function Car(props) {
   return <h2>I am a {props.age} Car!</h2>;
 }*/
+/*function Car(props) {
+  return <li>I am a { props.brand }</li>;
+}
+
+function Garage() {
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <ul>
+        {cars.map((car) => <Car key={car.id} brand={car.brand} />)}
+      </ul>
+    </>
+  );
+}*/
   export default Car;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Football from './Football';
+// import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
+// import Car from './Car';
+// import Garage from "./Car";
+// import Goal from './score';
+import MyForm from './myForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Football />);
+root.render(<MyForm />);
+/*
+Click F12 and navigate to the "Console view"
+to see the result when you submit the form.
+*/
+// root.render(<Goal isGoal={true} />);
+// root.render(<Goal isGoal={false} />);
+// root.render(<Garage />); 
 // root.render(<Car />);
 // root.render(<Car age='new' />);
