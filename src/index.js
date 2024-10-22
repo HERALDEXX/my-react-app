bed7e37961c4532990b380495abcb36a8de6baeb
HERALDEXX <inyangheraldmbre@outlook.com>
2024-10-22 15:10:58 +0100

    Installed React Router

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
 import ReactDOM from "react-dom/client";
-// import Car from './Car';
-// import Garage from "./Car";
-// import Goal from './score';
-import MyForm from './myForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
-/*
-Click F12 and navigate to the "Console view"
-to see the result when you submit the form.
-*/
-// root.render(<Goal isGoal={true} />);
-// root.render(<Goal isGoal={false} />);
-// root.render(<Garage />); 
-// root.render(<Car />);
-// root.render(<Car age='new' />);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/pages/NoPage.js
@@ -0,0 +1,5 @@
+const NoPage = () => {
+  return <h1>OOPS!!, This Page does not exist</h1>;
+};
+
+export default NoPage;
